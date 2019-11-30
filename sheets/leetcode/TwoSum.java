cfebf54979c0eba330e7653df60577e7aef55492
Shubham Gupta <shubham.sg2901@gmail.com>
2019-11-30 23:17:17 +0530

    Update and rename 0001.TwoSum.java to TwoSum.java

--- sheets/leetcode/TwoSum.java
+++ sheets/leetcode/TwoSum.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 
 /**
+ * https://leetcode.com/problems/two-sum/
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
  *
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -12,7 +13,7 @@ import java.util.HashMap;
  * Because nums[0] + nums[1] = 2 + 7 = 9,
  * return [0, 1].
  */
-class Solution {
+class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         for(int i = 0; i<nums.length; i++){
